72ee056afb7a872c56f8322585abe63f8e39d4ab
Ilianikonov <nikonov.ilia2013@yandex.ru>
2023-12-13 14:06:23 +0300

    r

---
+++ src/test/java/com/example/seasonservice/SeasonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seasonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
